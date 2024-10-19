ef62314e96c13f7c964c248689e8ae8bc33016be
samiam120 <samassefa3@gmail.com>
2024-10-19 09:00:56 -0700

    TaskManger manages our tasks rather than indivudually in our Tasks and Projects

---
+++ src/TaskManager.js
@@ -0,0 +1,21 @@
+export function createTaskManager(){
+    let _tasks = [];
+
+    function addTask(task){
+        _tasks.push(task);
+    }
+
+    function deleteTask(index){
+        _tasks.splice(index, 1);
+    }
+
+    function listTasks(){
+       return  _tasks.map(task => task.printTask());
+    }
+
+    return{
+        addTask,
+        deleteTask,
+        listTasks
+    }
+}
