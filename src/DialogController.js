402f1add51928585db077f081a5187b0f47c2ad1
samiam120 <samassefa3@gmail.com>
2024-10-31 21:24:44 -0700

    moved dialog logic to separate module

---
+++ src/DialogController.js
@@ -0,0 +1,31 @@
+function openDialog(dialog) {
+  dialog.showModal();
+}
+
+function closeDialog(dialog) {
+  dialog.close();
+}
+
+//event listener for opening add task
+export function dialogController() {
+  taskDialog();
+  projectDialog();
+}
+
+function taskDialog() {
+  const taskDialog = document.querySelector("#task-Dialog");
+  const addTaskBtn = document.querySelector(".addTaskBtn");
+  const closeBtn = document.querySelector(".close");
+
+  addTaskBtn.addEventListener("click", () => openDialog(taskDialog));
+  closeBtn.addEventListener("click", () => closeDialog(taskDialog));
+}
+
+function projectDialog() {
+  const projectDialog = document.querySelector("#project-Dialog");
+  const addProject = document.querySelector("#add-Project");
+  const closeProject = document.querySelector(".closeProject");
+
+  addProject.addEventListener("click", () => openDialog(projectDialog));
+  closeProject.addEventListener("click", () => closeDialog(projectDialog));
+}
