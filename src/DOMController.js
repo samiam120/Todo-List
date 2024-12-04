d8f62427c686e811d192e20e400e695b79cca285
samiam120 <samassefa3@gmail.com>
2024-12-04 11:49:44 -0800

    rendering projects and tasks

--- src/DOMController.js
+++ src/DOMController.js
@@ -1,13 +1,72 @@
 import { closeDialog } from "./DialogController";
 import projectManager from "./ProjectManager";
 import { createProject } from "./Projects";
+import { createTask } from "./Task";
 
 let selectedId = 0;
 
 const inbox = createProject("Inbox");
 projectManager.addProject(inbox);
+const task1 = createTask("task 1", "description 1", "09/09/2025", "low", false);
+const task2 = createTask("task 2", "description 2", "09/09/2025", "low", false);
+const task3 = createTask("task 3", "description 2", "09/09/2025", "low", false);
 
-export function renderProjects() {
+projectManager.addTaskToProject(0, task1);
+projectManager.addTaskToProject(0, task2);
+projectManager.addTaskToProject(0, task3);
+
+function renderTasks() {
+  const currentProject = projectManager.getProjectById(selectedId);
+  const listOfTasks = currentProject.getProjectContent().tasks;
+  console.log(listOfTasks);
+
+  const taskContainer = document.querySelector(".taskList");
+  taskContainer.textContent = "";
+  for (let i = 0; i < listOfTasks.length; i++) {
+    const div = document.createElement("div");
+    const li = document.createElement("li");
+    li.textContent = listOfTasks[i].title;
+    div.appendChild(li);
+    taskContainer.append(div);
+  }
+}
+
+function addTaskListener() {
+  const addTaskBtn = document.querySelector(".addTask");
+  addTaskBtn.addEventListener("click", () => {
+    const title = document.querySelector("#title");
+    const description = document.querySelector("#description");
+    const dueDate = document.querySelector("#dueDate");
+    const priority = document.querySelector("#priority");
+    const checkList = false;
+
+    if (title.value) {
+      const task = createTask(
+        title.value,
+        description.value,
+        dueDate.value,
+        priority.value,
+        checkList
+      );
+
+      projectManager.addTaskToProject(selectedId, task);
+      title.value = "";
+      description.value = "";
+      dueDate.value = "";
+      priority.value = "";
+      renderTasks();
+      closeDialog(document.querySelector("#task-Dialog"));
+      console.log(task);
+      console.log(
+        projectManager.getProjectById(selectedId).getProjectContent().tasks
+      );
+    } else {
+      console.log("Task title required");
+    }
+  });
+}
+
+function renderProjects() {
   const ul = document.querySelector(".project-list");
   ul.textContent = "";
   for (let [key, project] of projectManager.entries()) {
@@ -18,6 +77,8 @@ export function renderProjects() {
 
     li.addEventListener("click", () => {
       selectedId = key;
+      console.log(selectedId);
+      renderTasks();
     });
     console.log(li.dataset.id);
 
@@ -44,11 +105,8 @@ function addProjectListener() {
 }
 
 export function setProjectEventListeners() {
+  addTaskListener();
+  renderTasks();
+  renderProjects();
   addProjectListener();
-}
-
-export function addTaskToProject(projectId, task) {}
-
-export function renderTasks(projectId) {
-  const proj = projectManager.getProjectById(projectId);
 }
