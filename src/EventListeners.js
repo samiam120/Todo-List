4c30eab46648548d54a232a25180443411b19eac
samiam120 <samassefa3@gmail.com>
2024-12-09 20:04:08 -0800

    removed eventLisenters class

--- src/EventListeners.js
+++
@@ -1,7 +0,0 @@
-import { appendProject } from "./DOMController";
-
-export function initEventListeners() {
-  const addProjectToList = document.querySelector("#project-dialog-btn");
-
-  addProjectToList.addEventListener("click", () => appendProject());
-}
