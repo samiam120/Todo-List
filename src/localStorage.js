c6056492349a7ee16216c3b3257c805094dd843e
samiam120 <samassefa3@gmail.com>
2024-12-25 14:36:46 -0800

    on empty project, load default projects/tasks

--- src/localStorage.js
+++ src/localStorage.js
@@ -1,9 +1,11 @@
 import projectManager from "./ProjectManager";
 import { createProject } from "./Projects";
-import { createTask} from "./Task";
+import { createTask } from "./Task";
 class LocalStorage {
-  static save(){
-    const projList = Array.from(projectManager.values().map(project => project.getProjectContent()));
+  static save() {
+    const projList = Array.from(
+      projectManager.values().map((project) => project.getProjectContent())
+    );
     localStorage.setItem("projects", JSON.stringify(projList));
   }
 
@@ -13,16 +15,53 @@ class LocalStorage {
       const project = createProject(proj.name);
       project.setId(proj.id);
       proj.tasks.forEach((task) => {
-        const newTask = createTask(task.title, task.description, task.dueDate, task.priority, task.checkList);
+        const newTask = createTask(
+          task.title,
+          task.description,
+          task.dueDate,
+          task.priority,
+          task.checkList
+        );
         newTask.setId(task.id);
         project.addTaskToProject(newTask);
       });
       projectManager.addProject(project);
     });
+
+    if (projList.length === 0) {
+      const defaultProject = createProject("Inbox");
+      defaultProject.setId(0);
+      projectManager.addProject(defaultProject);
+
+      const defaultTask = createTask(
+        "Default Task",
+        "Default Description",
+        "01/01/2023",
+        "Low",
+        []
+      );
+      defaultTask.setId(0);
+      defaultProject.addTaskToProject(defaultTask);
+
+      const defaultTask2 = createTask(
+        "Walk the dog",
+        "Take the dog for a walk",
+        "2023-03-15",
+        "medium"
+      );
+      defaultTask2.setId(2);
+      defaultProject.addTaskToProject(defaultTask2);
+
+      const defaultTask3 = createTask(
+        "Do laundry",
+        "Do laundry today",
+        "2023-03-15",
+        "high"
+      );
+      defaultTask3.setId(3);
+      defaultProject.addTaskToProject(defaultTask3);
+    }
   }
 }
 
-export default LocalStorage; 
-
-//project requires we have a title, and we're given an id along with a task array
-//task requires we have a title, description, due date, priority, and check list
+export default LocalStorage;
